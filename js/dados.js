b04791549e0b7eae1d887d26236954ef184b7cce
Jessica-Silvaj <jessicasilva.jesusOfc@gmail.com>
2026-03-16 22:59:16 -0300

    Primeiro commit

---
+++ js/dados.js
@@ -0,0 +1,31 @@
+var MENU = {
+    "doces": [
+        {
+            "id": "cookies-chocolate",
+            "img": "./img/cardapio/doces/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
+            "name": "Cookies de Chocolate",
+            "dsc": "Biscoitos crocantes com gotas de chocolate belga, macios por dentro",
+            "price": 0
+        },
+        {
+            "id": "bolo-morango",
+            "img": "./img/cardapio/doces/bolo-de-morango-com-suspiros.jpeg",
+            "name": "Bolo de Morango",
+            "dsc": "Bolo macio com camadas de creme de morango e cobertura de suspiros",
+            "price": 0
+        },
+        {
+            "id": "brigadeiros",
+            "img": "./img/cardapio/doces/sabores-de-brigadeiros-em-uma-caixa.jpg",
+            "name": "Brigadeiros",
+            "dsc": "Brigadeiros artesanais em diversos sabores: chocolate, morango, pistache e doce de leite",
+            "price": 0
+        },
+    ],
+    "salgados": [
+       
+    ],
+    "bebidas": [
+
+    ]
+};
